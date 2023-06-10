869cf77e0a653b53b650c4f12bbe65cc962cc1ba
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2023-06-10 20:01:54 +0300

    para harcama finished

--- ParaHarcama/paraharcama/src/App.js
+++ ParaHarcama/paraharcama/src/App.js
@@ -3,16 +3,35 @@ import Header from './components/Header';
 import { useEffect, useState } from 'react';
 import products from './products.json'
 import Product from './components/Product';
+import './index.css';
+import Basket from './components/Basket';
 
 function App() {
-  const [money, setMoney] = useState(100);
+
+  const [money, setMoney] = useState(1000000) //state tanımlandı
+  const [basket, setBasket] = useState([])
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    setTotal(
+      basket.reduce((acc, item) => {
+        return acc + (item.amount * (products.find(product => product.id === item.id).price))
+      }, 0),
+    )
+
+  }, [basket])
+
   return (
-    <div >
-      <Header money={money} />
-      {products.map(product =>
-        <Product product={product} />
+    <>
+      <Header total={total} money={money} />
+      <div className='container products'>
+        {products.map(product => (
+          <Product key={product.id} total={total} money={money} basket={basket} setBasket={setBasket} product={product} />
+        ))}</div>
+      {total > 0 && (
+        <Basket setBasket={setBasket} total={total} products={products} basket={basket} />
       )}
-    </div>
+    </>
   );
 }
 
--- ParaHarcama/paraharcama/src/components/Header.js
+++ ParaHarcama/paraharcama/src/components/Header.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { moneyFormat } from '../helper';
 
-export default function Header({money}) {
-  return (
-    <div>
-        Harcamak için {money} $ paranız var!
-    </div>
-  )
+function Header({ money, total }) { //app.js ten money props u alındı
+   return (
+      < >
+         {total > 0 && money - total !== 0 && (
+            <div className='header'> Harcayacak <span>${moneyFormat(money - total)} </span> paranız kaldı!</div>
+         )}
+         {total === 0 && (
+            <div className='header'>Harcamak için <span> ${moneyFormat(money)}</span> paranız var!</div>
+         )}
+         {money - total === 0 && (
+            <div className='header'>Paranız bitti!!!</div>
+         )}
+         <style jsx>{`
+         .header{
+            background:linear-gradient(to bottom, #20b820, #14be2a);
+            height:60px;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            color:#fff;
+            font-size:24px;
+            letter-spacing:1px;
+            position:sticky;
+            top:0;
+         }
+         .header span{
+            margin:0 10px;
+            font-weight:bold;
+
+         }
+         `}</style>
+      </>
+   )
 }
+
+export default Header
